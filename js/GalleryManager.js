0a4aa671597e28a74982a689cb05789c8444f069
Itayi Patrick Chitovoro <patrick@chitosystems.com>
2018-08-07 13:44:43 +0200

    batch code update - v1 gallery

--- js/GalleryManager.js
+++ js/GalleryManager.js
@@ -97,7 +97,6 @@
                                     });
 
 
-<<<<<<< HEAD
 
 
 
@@ -129,27 +128,6 @@
                                     }
                                 });
 
-=======
-                                    $('.popup-gallery').magnificPopup({
-                                        type: 'image',
-                                        tLoading: 'Loading image #%curr%...',
-                                        mainClass: 'mfp-img-mobile',
-                                        gallery: {
-                                            enabled: true,
-                                            navigateByImgClick: true,
-                                            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
-                                        },
-                                        image: {
-                                            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-                                            titleSrc: function (item) {
-                                                return item.el.attr('title');
-                                            }
-                                        }
-                                    });
-
-
-                                }
->>>>>>> 2cff69437cc3058a3565b3bb05b9e30133ae7922
                             });
                         },
                         error: function () {
