b04aef26a12c58b7cb64bc5fd89628c79d6ee158
Itayi Patrick Chito-voro <patrick@chitosystems.com>
2020-06-25 10:04:43 +0200

    batch code update - v1 gallery

--- src/GalleryImage.php
+++ src/GalleryImage.php
@@ -2,94 +2,95 @@
 
 class GalleryImage extends DataObject
 {
-    
-    public static  $default_sort   = 'SortOrder';
-    private static $db             = array(
+
+    public static $default_sort = 'SortOrder';
+    private static $db = [
         'Name'        => 'Varchar(255)',
         'Description' => 'Text',
         'VideoCode'   => 'Varchar(255)',
         'SortOrder'   => 'Int',
-    );
-    private static $has_one        = array(
+    ];
+    private static $has_one = [
         'Attachment'  => 'Image',
         'GalleryPage' => 'GalleryPage',
+        'Page'        => 'Page',
         'Gallery'     => 'Gallery',
-    );
-    private static $has_many       = array(
+    ];
+    private static $has_many = [
         'Images' => 'GallerySubImage',
-    );
-    private static $summary_fields = array(
+    ];
+    private static $summary_fields = [
         'Thumbnail',
         'Name',
-    );
-    
+    ];
+
     public function ChildImageList()
     {
-        
+
         $images = $this->Images();
-        if ( count( $images ) ) {
-            $AttachmentIDs = $images->column( "AttachmentID" );
-            $images        = Image::get()->byIDs( $AttachmentIDs );
-            
-            return implode( ',', $images->column( "Filename" ) );
+        if ( count($images) ) {
+            $AttachmentIDs = $images->column("AttachmentID");
+            $images = Image::get()->byIDs($AttachmentIDs);
+
+            return implode(',', $images->column("Filename"));
         }
-        
+
         return false;
     }
-    
+
     public function getCMSFields()
     {
-        
+
         $f = parent::getCMSFields();
-        $f->removeByName( [
+        $f->removeByName([
             "SortOrder",
             "GalleryPageID",
-        ] );
-        
+        ]);
+
         $gridFieldConfig = GridFieldConfig_RecordEditor::create();
-        $gridFieldConfig->addComponent( new GridFieldBulkUpload() );
-        $gridFieldConfig->addComponent( new GridFieldSortableRows( 'SortOrder' ) );
-        $gridFieldConfig->getComponentByType( 'GridFieldBulkUpload' )
-                        ->setUfSetup( 'setFolderName', 'Uploads/galleryimages/images/' . $this->URLSegment );
-        $gridfield = new GridField( 'Images', 'Images', $this->Images(), $gridFieldConfig );
-        $f->addFieldToTab( 'Root.Images', $gridfield );
-        
-        
+        $gridFieldConfig->addComponent(new GridFieldBulkUpload());
+        $gridFieldConfig->addComponent(new GridFieldSortableRows('SortOrder'));
+        $gridFieldConfig->getComponentByType('GridFieldBulkUpload')
+            ->setUfSetup('setFolderName', 'Uploads/galleryimages/images/' . $this->URLSegment);
+        $gridfield = new GridField('Images', 'Images', $this->Images(), $gridFieldConfig);
+        $f->addFieldToTab('Root.Images', $gridfield);
+
+
         return $f;
     }
-    
+
     public function SegmentFilter()
     {
-        
+
         //   $parent = ($pId = $this->GalleryPageID) ? $this->GalleryPage() : $this->Gallery();
         if ( $this->GalleryPageID ) {
             $parent = $this->GalleryPage();
-            
+
             return $parent->URLSegment;
         }
-        
+
         return false;
     }
-    
+
     public function getThumbnail()
     {
-        
+
         $image = $this->Attachment();
         if ( $image && $image->ID ) {
             return $image->CMSThumbnail();
         }
     }
-    
+
     public function Image()
     {
-        
+
         $img = $this->Attachment();
         if ( $img && $img->ID ) {
-            return $img->newClassInstance( 'GalleryFittedImage' );
+            return $img->newClassInstance('GalleryFittedImage');
         }
-        $this->Content = str_replace( "http://", "//", $this->Content );
-        
+        $this->Content = str_replace("http://", "//", $this->Content);
+
         return false;
     }
-    
+
 }
